a6e91208bef2373ddabe5180b38c0201c8668a0d
Mateus998 <mateus@LAPTOP-ODMRT7KF.>
2026-03-17 15:18:26 +0000

    modulo 00 end

--- Py00/ex10/loading.py
+++ Py00/ex10/loading.py
@@ -3,19 +3,36 @@ import time
 bar_width = 40
 
 def ft_progress(list):
+    start = time.perf_counter()
+    total = len(list)
     for i, l in enumerate(list, start=1):
 
-        ratio = i / len(list)
+        ratio = i / total
         filled = int(ratio * bar_width)
         empty = bar_width - filled
 
-        percent = "[" + str(ratio * 100) + "%]"
-        bar = "[" + ("=" * filled) + (" " * empty) + "]"
-        print(f"\r{percent} {bar}", end="")
+        percent = str(int(ratio * 100))
+        if empty != 0:
+            bar = ("=" * filled) + ">" + (" " * (empty - 1))
+        else:
+            bar = ("=" * filled) + (" " * empty)
+
+        n_total = str(i) + '/' + str(total)
+
+        elapsed_time = time.perf_counter() - start
+
+        if i == 0:
+            eta = '-'
+        else:
+            rate = elapsed_time / i
+            remaining = total - i
+            eta = remaining * rate
+            
+        print(f"\rETA: {eta:.2f}s [{percent:>3}%] [{bar}] {n_total} | elapsed time {elapsed_time:.2f}s", end="")
 
         if ratio == 1:
-            print("\n")
+            print()
         yield l
 
 for x in ft_progress(range(200)):
-    time.sleep(0.01)
+    time.sleep(0.025)
